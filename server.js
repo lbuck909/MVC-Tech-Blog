95500928d160b330bd08c6198f7637cc2526900b
LaiSha Buck <laishabuck@gmail.com>
2023-05-28 20:33:34 -0400

    updates

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const path = require('path');
+
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/heplers');
@@ -9,7 +10,7 @@ const hbs = exphbs.create({ helpers });
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const session = require('express-session');
+
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
