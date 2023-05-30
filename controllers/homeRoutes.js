199bc7e0329e24992bf9a71c19330c1d81b91032
LaiSha Buck <laishabuck@gmail.com>
2023-05-30 15:37:58 -0400

    homeroutes round 1

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,2 +1,18 @@
 const router = require('express').Router();
 const {Post, User, Comment} = require('../models');
+const sequelize = require('../config/connection');
+
+//get all posts from homepage
+
+router.get('/', async (req, res) =>
+{
+  try {
+    //all posts and user data
+    const postData = await Pos
+  }
+})
+
+
+// get single post
+
+//get login 
