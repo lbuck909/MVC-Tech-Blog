60378d5ea2d36e8b6d3b1c9396b8db5fc909fdb3
LaiSha Buck <laishabuck@gmail.com>
2023-06-21 20:18:23 -0400

    comment route updated

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -3,3 +3,16 @@ const sequelize = require('../config/connection');
 const { Comment } = require('../../models');
 const withAuth = require('../utils/auth');
 
+router.post('/:id', async (req, res) => {
+  try{
+    const newComment = await Comment.create({
+      ...req.body,
+      user_id: req.session.user_id,
+      post_id: req.params.id
+    });
+    res.json({newComment})
+  }catch (err) {
+    res.status(500).json(err);
+  }
+})
+module.exports = router;
