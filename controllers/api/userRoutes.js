cc614a0e40f0fabfa3cfa18b552f8909da317ab5
LaiSha Buck <laishabuck@gmail.com>
2023-06-28 19:45:12 -0400

    new user info created added

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,3 +1,33 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
-module.exports = router
+
+//New user info created
+router.post('/', async (req, res) =>{
+  try{
+    const newUser = await User.create({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    req.session.user_id = newUser.id;
+    req.session.username = newUser.username;
+    req.session.loggedIn = true;
+
+    req.session.save(() =>{
+      res.json(newUser);
+    });
+  }catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+//login 
+
+
+//logout
+
+
+
+
+
+module.exports = router;
