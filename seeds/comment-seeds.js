d38fa9e71319cc319e64755ae8a672470c0785bf
LaiSha Buck <laishabuck@gmail.com>
2023-07-04 19:12:47 -0400

    post seeds created

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -13,22 +13,22 @@ const commentSeeds = [
   },
   {
     user_id: 3,
-    post_id: 1,
+    post_id: 2,
     comment_text: "Where could I find more information?"
   },
   {
     user_id: 4,
-    post_id: 2,
+    post_id: 1,
     comment_text: "I do not agree at all. I'm offended by your remarks"
   },
   {
     user_id:4, 
-    post_id: 1,
+    post_id: 5,
     comment_text: "Nice job stating the obvious"
   },
   {
     user_id: 3,
-    post_id: 3,
+    post_id: 6,
     comment_text: "Very well written"
   },
   {
---
+++ seeds/post-seeds.js
@@ -0,0 +1,43 @@
+const { Post } = require('../models');
+
+const postSeeds = [
+  {
+    user_id:1,
+    title: "Toddlers that lift weights"
+    post_content: "Studies how that toddlers who lift weights 3 days a week eat more broccoli than their no lifting counterparts."
+  },
+  {
+    user_id:2,
+    title: "Vegan Cheese, why bother?!"
+    post_content: "Most vegand enjoy nutritional yeast to add a slighty cheesey flavor to most foods. The artifical cheese slices taste like plastic most days."
+  },
+  {
+    user_id:3,
+    title: "Where in the world is Waldo?"
+    post_content: "The icon is on his fifth marriage. He plans to start a podcast on successful marriage tips"
+  },
+  {
+    user_id:4,
+    title: "Fake meat is coming to a takeout near you"
+    post_content: " Processed foods have been around for decades, should we even be shocked at this point?"
+  },
+  {
+    user_id:5,
+    title: "Population control sponosred by Bill Gates"
+    post_content: "Gates gave a TED talk on reducing the population. We will never know how true this is until we cannot find our friends anymore"
+  },
+  {
+    user_id:6,
+    title: "The last 20 pounds"
+    post_content: "I've lost 20 pounds at least fives in my life. Does that count as me losing 100 pounds?"
+  },
+  {
+    user_id:7,
+    title: "Dad jokes or dad bods?"
+    post_content: " Both can spice up the carpool line! "
+  }
+]
+
+const postSeeds = () => Post.bulkCreate(postData);
+
+module.exports = postSeeds;
